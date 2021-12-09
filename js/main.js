7e56c30efb27a277fd524662a3017734d67e0de4
Richard-EH <83168110+Richard-EH@users.noreply.github.com>
2021-12-08 22:07:05 -0500

    Search Bar up and running
    Added search bar to Leaflet Map

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ function createMap() {
     }).setView([44.7,-90],6.5);
     //Attempting to Troubleshoot this line, I can't figure out why this is being deemed to not be a funtion when it is a plugin. 
     //L.control.NavBar().addTo(map);
+    L.Control.geocoder().addTo(map);
     
     
 
