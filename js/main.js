52a3e7851d344824b9bc88b71ca1a8536ab37187
Richard-EH <83168110+Richard-EH@users.noreply.github.com>
2021-12-15 20:40:05 -0500

    Code Cleanup
    Elimination of commented out code in main.js.

--- js/main.js
+++ js/main.js
@@ -19,24 +19,6 @@ function createMap() {
      }).addTo(map);
 
 
-    // commented out to experiment with base layers.
-    // //add forest green mapbox basemap tilelayer
-    // var forest = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', { attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://mapbox.com">Mapbox</a>', maxZoom: 18, id: 'emilyjo/ckuk9l61v7x8y19nybp6l6rhb', accessToken:'pk.eyJ1IjoiZW1pbHlqbyIsImEiOiJjazA2MDI4MjEwNWM2M3BxYm42N29rMDNoIn0.TCOSlqzEBB-uE8PDYUq4OA'}).addTo(map);
-
-    // //add osm basemap tilelayer
-    // var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    // });
-
-    // //define basemaps and add switcher control
-    // var basemaps = {
-    //     "Dark": forest,
-    //     "Light": osm
-    // };
-
-    // L.control.layers(basemaps).addTo(map);
-    
-
     //call getData function
     getData(map);
 };
